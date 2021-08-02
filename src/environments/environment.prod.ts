4731666126aedbff3e5842943cf4a74f63265d57
Helio Gonzales Rivas <heliogonzalesr@gmail.com>
2021-08-02 18:52:01 +0200

    Stripe PK added to prod-enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,4 +8,6 @@ export const environment = {
     messagingSenderId: '69350725110',
     appId: '1:69350725110:web:a05e4405822e2d7dc123d7',
   },
+  stripe_PK:
+    'pk_test_51JHT3UJmyRdrNKt2fUkmluzffH3NXlh6Rhuj9cswFKJTvZUFNZUcxoNIzUx6hJMicIr5ZalD5POInFqCLL0Nl0VG00ZnGE6dh3',
 };
